36d4dfaa64802aa06fadb20e07e29c88a3377591
alex31bel <alex@31bel.ru>
2023-04-29 20:19:24 +0300

    add file terraform

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "${var.token_ya}"
+  cloud_id  = "${var.cloud_id_ya}"
+  folder_id = "${var.folder_id_ya}"
+  zone      = "ru-central1-a"
+}
