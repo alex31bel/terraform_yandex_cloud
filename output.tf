7831acd588adcdbeecce8c6871898cfc2f59915c
alex31bel <alex@31bel.ru>
2023-05-01 16:08:35 +0300

    fix output

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "instance_external_ip" {
-  value = "${data.yandex_compute_instance_group.ig-1.instances.*.network_interface.0.nat_ip_address}"
+  value = "${yandex_compute_instance_group.ig-1.instances.*.network_interface.0.nat_ip_address}"
 }
