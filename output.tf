f544620b23c66a1f153361b39302679c4382cc40
alex31bel <alex@31bel.ru>
2023-05-01 16:06:29 +0300

    fix output

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "internal-group-ig-1" {
-  value = "${yandex_compute_instance_group.ig-1.network_interface.0.nat_ip_address}"
+output "instance_external_ip" {
+  value = "${data.yandex_compute_instance_group.ig-1.instances.*.network_interface.0.nat_ip_address}"
 }
