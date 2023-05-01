f1f348bee556a5783dcd42ae0de7637523cf1f6d
alex31bel <alex@31bel.ru>
2023-05-01 14:50:21 +0300

    task 2

--- main.tf
+++ main.tf
@@ -13,74 +13,84 @@ provider "yandex" {
   zone      = "ru-central1-a"
 }
 
-resource "yandex_compute_instance" "vm" {
-  count = 2
+resource "yandex_iam_service_account" "ig-sa" {
+  name        = "mashkovad"
+  description = "service account to manage mashkovad"
+}
 
-  name        = "vm${count.index}"
-  platform_id = "standard-v3"
+resource "yandex_resourcemanager_folder_iam_member" "editor" {
+#  folder_id = var.folder_id_ya
+  role      = "editor"
+  member   = "serviceAccount:${yandex_iam_service_account.ig-sa.id}"
+}
 
-  resources {
-    cores  = "2"
-    memory = "2"
-    core_fraction = "20"
-  }
+resource "yandex_compute_instance_group" "ig-1" {
+  name               = "fixed-ig-with-balancer"
+#  folder_id          = var.folder_id_ya
+  service_account_id = "${yandex_iam_service_account.ig-sa.id}"
+  instance_template {
+    platform_id = "standard-v3"
+    resources {
+      memory = 2
+      cores  = 2
+    }
 
-  boot_disk {
-    initialize_params {
-      image_id = "${var.image_id_ya}"
-      size = 3
+    boot_disk {
+      mode = "READ_WRITE"
+      initialize_params {
+        image_id = var.image_id_ya
+      }
+    }
+
+    network_interface {
+      network_id = "${yandex_vpc_network.network-1.id}"
+      subnet_ids = ["${yandex_vpc_subnet.subnet-1.id}"]
+    }
+
+    metadata = {
+      user-data = file("./main.yaml")
+    }
+
+    scheduling_policy {
+    preemptible = true #Создание прерываемой ВМ для экономии баланса при обучении
     }
   }
 
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
-    nat       = true
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
   }
 
-  metadata = {
-    user-data = file("./main.yaml")
+  allocation_policy {
+    zones = ["ru-central1-a"]
   }
 
-  scheduling_policy {
-    preemptible = true #Создание прерываемой ВМ для экономии баланса при обучении
+  deploy_policy {
+    max_unavailable = 1
+    max_expansion   = 0
   }
-}
 
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  v4_cidr_blocks = ["192.168.10.0/24"]
-  network_id     = "${yandex_vpc_network.network-1.id}"
-}
-
-#Создание целевой группы Network Load Balancer
-resource "yandex_lb_target_group" "target-1" {
-  name      = "target1"
-  target {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    address   = yandex_compute_instance.vm[0].network_interface.0.ip_address
-  }
-  target {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    address   = yandex_compute_instance.vm[1].network_interface.0.ip_address
+  load_balancer {
+    target_group_name        = "target-group"
+    target_group_description = "load balancer target group"
   }
 }
 
-#Создание сетевого балансировщика
 resource "yandex_lb_network_load_balancer" "lb-1" {
-  name = "lb1"
+  name = "network-load-balancer-1"
+
   listener {
-    name = "my-list"
+    name = "network-load-balancer-1-listener"
     port = 80
     external_address_spec {
       ip_version = "ipv4"
     }
   }
+
   attached_target_group {
-    target_group_id = yandex_lb_target_group.target-1.id
+    target_group_id = yandex_compute_instance_group.ig-1.load_balancer.0.target_group_id
+
     healthcheck {
       name = "http"
       http_options {
@@ -89,10 +99,14 @@ resource "yandex_lb_network_load_balancer" "lb-1" {
       }
     }
   }
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
 }
 
-#Создание снимка
-resource "yandex_compute_snapshot" "snapshot-1" {
-  name           = "snapshot1"
-  source_disk_id = yandex_compute_instance.vm[0].boot_disk[0].disk_id
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+#  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.network-1.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
