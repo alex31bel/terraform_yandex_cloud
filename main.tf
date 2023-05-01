a1cab99fbd69fb93fe58f601aae7099de6b3df74
alex31bel <alex@31bel.ru>
2023-05-01 15:13:48 +0300

    fix

--- main.tf
+++ main.tf
@@ -19,14 +19,14 @@ resource "yandex_iam_service_account" "ig-sa" {
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "editor" {
-#  folder_id = var.folder_id_ya
+  folder_id = var.folder_id_ya
   role      = "editor"
   member   = "serviceAccount:${yandex_iam_service_account.ig-sa.id}"
 }
 
 resource "yandex_compute_instance_group" "ig-1" {
   name               = "fixed-ig-with-balancer"
-#  folder_id          = var.folder_id_ya
+  folder_id          = var.folder_id_ya
   service_account_id = "${yandex_iam_service_account.ig-sa.id}"
   instance_template {
     platform_id = "standard-v3"
@@ -107,7 +107,7 @@ resource "yandex_vpc_network" "network-1" {
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
-#  zone           = "ru-central1-a"
+  zone           = "ru-central1-a"
   network_id     = "${yandex_vpc_network.network-1.id}"
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
